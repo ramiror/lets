b4c35ab2b4cc4012c15782a6d6ecb2dd6409f4ef
Ramiro Rela <ramiro.rela+git@gmail.com>
2012-11-11 09:27:15 -0300

    Intento apurado de meter estilos robados cof cof inspirados en airbnb

---
+++ index.php
@@ -0,0 +1,23 @@
+<html>
+  <link rel="stylesheet" type="text/css" href="meetto.css" media="screen"/>
+  <link rel="stylesheet" type="text/css" href="styles.css" media="screen"/>
+  <link rel="stylesheet" type="text/css" href="styles2.css" media="screen"/>
+  <div id="main" class="main">
+    <div id="header" class="centered">
+      <img src="images/header.png"/>
+    </div>
+    <div id="form" class="show-search-options">
+      <input type="text" id="what" class="sancocho checkout search-option ui-datepicker-target" name="what" placeholder="What do you want to do?">
+      <input type="text" id="where" class="checkout search-option ui-datepicker-target" name="where" placeholder="Where?">
+      <input type="text" id="when" class="checkout search-option ui-datepicker-target" name="when" placeholder="When?">
+      <span class="search-area-icon search-area-icon-checkout">
+        <img src="images/search.png"/>
+      </span>
+    </div>
+    <div id="results">
+    </div>
+  </div>
+  <div id="categories" class="categories">
+    <img src="images/categories.png"/>
+  </div>
+</html>
